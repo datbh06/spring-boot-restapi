c6ffee8d46bf4bda7155b348a39e08c541af57d4
datbh06 <bdat1606@gmail.com>
2023-09-21 01:33:20 +0700

    feat: Customizing Validation Response

--- src/main/java/com/yugen/springbootrestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yugen/springbootrestapi/exception/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package com.yugen.springbootrestapi.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Global exception handler for the application.
  */
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      * Handles ResourceNotFoundExceptions throughout the application.
@@ -66,5 +73,27 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Handles exceptions that are thrown when method arguments are not valid.
+     *
+     * @param ex      the exception that was thrown
+     * @param headers the headers to be included in the response
+     * @param status  the HTTP status of the response
+     * @param request the current web request
+     * @return a ResponseEntity containing a map of field errors and their corresponding error messages
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request) {
 
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldError = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldError, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
