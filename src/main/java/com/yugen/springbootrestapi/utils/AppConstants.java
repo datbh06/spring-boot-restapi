4fa06ce18582f45d9d314d34ca080df3d2a8d187
datbh06 <bdat1606@gmail.com>
2023-09-20 15:54:46 +0700

    docs: add java docs for more detailed AppConstants

--- src/main/java/com/yugen/springbootrestapi/utils/AppConstants.java
+++ src/main/java/com/yugen/springbootrestapi/utils/AppConstants.java
@@ -1,10 +1,27 @@
 package com.yugen.springbootrestapi.utils;
 
+/**
+ * Contains constant values used throughout the application.
+ */
 public class AppConstants {
 
+    /**
+     * Default page number used for pagination.
+     */
     public static final String DEFAULT_PAGE_NUMBER = "0";
-    public static final String DEFAULT_PAGE_SIZE = "10";
-    public static final String DEFAULT_SORT_BY = "id";
-    public static final String DEFAULT_SORT_ORDER = "asc";
 
+    /**
+     * Default page size used for pagination.
+     */
+    public static final String DEFAULT_PAGE_SIZE = "10";
+
+    /**
+     * Default field to sort by.
+     */
+    public static final String DEFAULT_SORT_BY = "id";
+
+    /**
+     * Default sort order.
+     */
+    public static final String DEFAULT_SORT_ORDER = "asc";
 }
