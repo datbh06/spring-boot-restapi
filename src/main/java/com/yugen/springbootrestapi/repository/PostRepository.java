34b43b374e2ef2677fad4c3771434b03272ba4fd
datbh06 <bdat1606@gmail.com>
2023-09-20 16:05:37 +0700

    refactor: remove unnecessary annotation
    @Repository

--- src/main/java/com/yugen/springbootrestapi/repository/PostRepository.java
+++ src/main/java/com/yugen/springbootrestapi/repository/PostRepository.java
@@ -2,8 +2,6 @@ package com.yugen.springbootrestapi.repository;
 
 import com.yugen.springbootrestapi.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
