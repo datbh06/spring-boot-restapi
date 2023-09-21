18265dd15d271a571df5a588bd078e36477b3af8
datbh06 <bdat1606@gmail.com>
2023-09-21 12:32:04 +0700

    docs: add java docs

--- src/main/java/com/yugen/springbootrestapi/repository/CommentRepository.java
+++ src/main/java/com/yugen/springbootrestapi/repository/CommentRepository.java
@@ -5,7 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Repository interface for managing comments in the database.
+ */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    /**
+     * Finds all comments associated with a specific post.
+     *
+     * @param postId the ID of the post
+     * @return a list of comments associated with the post
+     */
     List<Comment> findByPostId(Long postId);
 }
--- src/main/java/com/yugen/springbootrestapi/repository/PostRepository.java
+++ src/main/java/com/yugen/springbootrestapi/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.yugen.springbootrestapi.repository;
 import com.yugen.springbootrestapi.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing users in the database.
+ */
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
