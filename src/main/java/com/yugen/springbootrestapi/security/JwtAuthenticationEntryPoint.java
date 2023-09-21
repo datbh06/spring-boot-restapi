c20db341d181605d3537192cb46c7900d8a973f1
datbh06 <bdat1606@gmail.com>
2023-09-21 17:14:28 +0700

    docs: add java docs

--- src/main/java/com/yugen/springbootrestapi/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/yugen/springbootrestapi/security/JwtAuthenticationEntryPoint.java
@@ -9,9 +9,22 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * JwtAuthenticationEntryPoint is a component that provides an entry point for JWT authentication.
+ * It handles AuthenticationException by sending an unauthorized error response.
+ */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    /**
+     * This method is invoked when there is an AuthenticationException.
+     *
+     * @param request       The HttpServletRequest.
+     * @param response      The HttpServletResponse.
+     * @param authException The AuthenticationException.
+     * @throws IOException      If an input or output exception occurred.
+     * @throws ServletException If a servlet exception occurred.
+     */
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
