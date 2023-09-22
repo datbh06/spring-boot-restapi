15ed970b7d0ab4b0a36d89576ef3a22ed4acda85
datbh06 <bdat1606@gmail.com>
2023-09-22 14:07:26 +0700

    feat: Define API Information using Annotations @OpenAPIDefinition

--- src/main/java/com/yugen/springbootrestapi/SpringBootRestapiApplication.java
+++ src/main/java/com/yugen/springbootrestapi/SpringBootRestapiApplication.java
@@ -1,11 +1,35 @@
 package com.yugen.springbootrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Blog App RestAPI",
+                description = "Spring Boot Documentation Blog API v1.0",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Datbh",
+                        email = "bdat1606@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Spring Boot Documentation Blog API v1.0",
+                url = "https://github.com/datbh06/spring-boot-restapi/tree/master"
+        )
+)
 public class SpringBootRestapiApplication {
 
     /**
